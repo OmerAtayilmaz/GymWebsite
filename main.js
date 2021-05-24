f3e40962aef7235be270405c2e25fae8c3a6e955
Ömer Atayilmaz <64100323+OmerAtayilmaz@users.noreply.github.com>
2021-05-24 16:38:53 +0300

    sign-up

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const http=require('http');
 const fs = require('fs');
 const page =fs.readFileSync(`${__dirname}/index.html`,'utf-8');
 
+
+
 const server = http.createServer();
 //CSS,IMG vs. hepsi nasıl page dahil edileceğini öğrendiğinde projeye devam edilecek.
 server.on("request",(req,res)=>{
